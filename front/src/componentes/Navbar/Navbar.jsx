2f3b7068d9aa523cffdcd8a1ab5a50dff86789b4
Carlos Octavio Otero <166446069+OteroCarlos1977@users.noreply.github.com>
2024-10-17 22:48:41 -0300

    Cambio Button
    Se realizan cambios en el componente button tooltip e icono

--- front/src/componentes/Navbar/Navbar.jsx
+++ front/src/componentes/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
-// src/componentes/Navbar/Navbar.jsx
-
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import { faSignInAlt, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
+import { Button} from '../Button/Button';
 import "./Navbar.css";
+
 
 // eslint-disable-next-line react/prop-types
 export function Navbar({ isLoggedIn, onLogoutClick }) {
@@ -18,13 +19,27 @@ export function Navbar({ isLoggedIn, onLogoutClick }) {
     }
   };
 
+  const buttonStyles = {
+    backgroundColor: 'transparent',
+    color: isLoggedIn ? 'red' : 'green', 
+    fontSize: '2rem',  
+    fontWeight: 'bolder',
+    border: 'black',
+    cursor: 'pointer',
+    padding: '10px 20px',
+    
+  };
+
   return (
     <nav className="navbar">
       <img src="/logo_Cast.png" alt="Logo" className="navbar-logo" /> {/* Asegúrate de que la ruta es correcta */}
       <h2>Turnero Hospital Municipal</h2>
-      <button className="login-icon" onClick={handleButtonClick}>
-        {isLoggedIn ? 'Logout' : 'Login'}
-      </button>
+      <Button
+      style={buttonStyles}
+      tooltip={isLoggedIn ? "Salir" : "Ingresar"}
+      icono={isLoggedIn ? faSignOutAlt : faSignInAlt}
+      onClick={handleButtonClick}
+      />
     </nav>
   );
 }
